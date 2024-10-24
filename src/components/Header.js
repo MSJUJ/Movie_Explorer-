fde9fa5f15398f493dc40a3b1bfb799d2c0d2f8a
MSJUJ <bscs22060@itu.edu.pk>
2024-10-24 09:38:13 +0500

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Movie Explorer</h1>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/favorites">Favorites</Link>
+    </nav>
+  </header>
+);
+
+export default Header;
