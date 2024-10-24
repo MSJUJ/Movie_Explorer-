087f72efd6316297cb1f474a11d2694c5398109c
MSJUJ <bscs22060@itu.edu.pk>
2024-10-24 09:53:05 +0500

    Favourite Context

---
+++ src/context/FavoritesContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+export const FavoritesContext = createContext();
+
+export const FavoritesProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const addFavorite = (movie) => setFavorites((prev) => [...prev, movie]);
+  const removeFavorite = (id) => setFavorites((prev) => prev.filter((m) => m.id !== id));
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
